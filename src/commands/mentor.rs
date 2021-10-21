c6ffd16201bead65254520e4610ff4961703d7a7
YWang <user@localhost.localdomain>
2021-10-21 05:43:12 +0100

    Improve and fix pair command

--- src/commands/mentor.rs
+++ src/commands/mentor.rs
@@ -1,62 +1,81 @@
-use std::fmt;
-
-use serenity::framework::standard::{macros::command, Args, CommandResult};
+use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::model::channel::{PermissionOverwrite, PermissionOverwriteType};
 use serenity::model::prelude::*;
-use serenity::model::{ModelError, Permissions};
+use serenity::model::Permissions;
 use serenity::prelude::*;
-use serenity::Result;
 
-async fn get_member(ctx: &Context, guild: &GuildId, name: &str) -> Result<Option<Member>> {
-    let candidates = guild.search_members(ctx, name, None).await?;
-    if candidates.len() == 1 {
-        Ok(Some(candidates[0].clone()))
-    } else {
-        Ok(None)
-    }
-}
+const MENTORING_CATEGORY_NAME: &str = "Mentoring";
 
 #[command]
-pub async fn pair(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+pub async fn pair(ctx: &Context, msg: &Message) -> CommandResult {
     msg.channel_id
         .say(&ctx.http, "Attempting to pair...")
         .await?;
     if let Some(guild) = msg.guild_id {
-        let mentor_name = args.single::<String>()?;
-        let mentee_name = args.single::<String>()?;
+        msg.channel_id.say(&ctx.http, "Acquired guild...").await?;
+        if let [mentor, mentee] = &msg.mentions[..] {
+            msg.channel_id.say(&ctx.http, "Parsed arguments...").await?;
+            if let Some(mentoring_category) = guild
+                .to_partial_guild(ctx)
+                .await?
+                .channel_id_from_name(ctx, MENTORING_CATEGORY_NAME)
+                .await
+            {
+                msg.channel_id
+                    .say(&ctx.http, "Found mentoring category...")
+                    .await?;
+                let id = 0;
+                let name = format!("{} - {}", mentor.name, mentee.name);
+                let chan_text = guild
+                    .create_channel(ctx, |c| {
+                        c.name(&name)
+                            .kind(ChannelType::Text)
+                            .category(mentoring_category)
+                    })
+                    .await?;
+                let chan_voice = guild
+                    .create_channel(ctx, |c| {
+                        c.name(&name)
+                            .kind(ChannelType::Voice)
+                            .category(mentoring_category)
+                    })
+                    .await?;
+                let perm_mentor = PermissionOverwrite {
+                    allow: Permissions::READ_MESSAGES,
+                    deny: Permissions::empty(),
+                    kind: PermissionOverwriteType::Member(mentor.id),
+                };
+                let perm_mentee = PermissionOverwrite {
+                    kind: PermissionOverwriteType::Member(mentee.id),
+                    ..perm_mentor
+                };
+                let perm_everyone = PermissionOverwrite {
+                    allow: Permissions::empty(),
+                    deny: Permissions::READ_MESSAGES,
+                    kind: PermissionOverwriteType::Role(RoleId(guild.0)),
+                };
+                chan_text.create_permission(ctx, &perm_mentor).await?;
+                chan_voice.create_permission(ctx, &perm_mentor).await?;
+                chan_text.create_permission(ctx, &perm_mentee).await?;
+                chan_voice.create_permission(ctx, &perm_mentee).await?;
+                chan_text.create_permission(ctx, &perm_everyone).await?;
+                chan_voice.create_permission(ctx, &perm_everyone).await?;
 
-        let id = 0;
-        if let (Some(mentor), Some(mentee)) = (
-            get_member(ctx, &guild, &mentor_name).await?,
-            get_member(ctx, &guild, &mentee_name).await?,
-        ) {
-            let name = format!("{},{}", mentor.display_name(), mentee.display_name());
-            let chan_text = guild
-                .create_channel(ctx, |c| c.name(&name).kind(ChannelType::Text))
-                .await?;
-            let chan_voice = guild
-                .create_channel(ctx, |c| c.name(&name).kind(ChannelType::Voice))
-                .await?;
-            let perm_mentor = PermissionOverwrite {
-                allow: Permissions::READ_MESSAGES,
-                deny: Permissions::empty(),
-                kind: PermissionOverwriteType::Member(mentor.user.id),
-            };
-            let perm_mentee = PermissionOverwrite {
-                kind: PermissionOverwriteType::Member(mentee.user.id),
-                ..perm_mentor
-            };
-            chan_text.create_permission(ctx, &perm_mentor).await?;
-            chan_voice.create_permission(ctx, &perm_mentor).await?;
-            chan_text.create_permission(ctx, &perm_mentee).await?;
-            chan_voice.create_permission(ctx, &perm_mentee).await?;
+                msg.channel_id
+                    .say(
+                        &ctx.http,
+                        format!(
+                            "Pair created (id = {id}):\n\
+                            Mentor: {}\n\
+                            Mentee: {}",
+                            mentor.mention(),
+                            mentee.mention(),
+                            id = id
+                        ),
+                    )
+                    .await?;
+            }
         }
-
-        let message = format!(
-            "Pair created: Mentor:{} Mentee:{} (id: {})",
-            mentor_name, mentee_name, id
-        );
-        msg.channel_id.say(&ctx.http, message).await?;
     }
 
     Ok(())
